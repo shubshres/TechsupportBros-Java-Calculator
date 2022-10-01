b8f0cd78b1a4a97d767e5b92e416e7a1f1896e8a
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2022-10-01 03:19:23 -0500

    Added some more features and tests

--- src/main/java/com/Calculator/calculator.java
+++ src/main/java/com/Calculator/calculator.java
@@ -1,60 +1,254 @@
 package com.Calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.lang.Math;
+
 
 public class calculator {
-    public static double add (double a, double b){
-        return (a + b);
+    public static double add (double[] numbers){
+        double total = 0;
+
+        for(int i = 0; i < numbers.length; i++){
+            total += numbers[i];
+        }
+
+        return (total);
     }
 
-    public static double multiply (double a, double b){
-        return (a * b);
+    public static double multiply (double[] numbers){
+        double total = 1;
+
+        for(int i = 0; i < numbers.length; i++){
+            total = numbers[i] * total;
+        }
+
+        return (total);
     }
 
-    public static double divide (double a, double b){
-        return (a / b);
+    public static double divide (double[] numbers){
+        double total = numbers[0];
+
+        for(int i = 1; i < numbers.length; i++){
+            total = total/numbers[i];
+        }
+
+        return (total);
     }
 
-    public static double subtract (double a, double b){
-        return (a - b);
+    public static double subtract (double[] numbers){
+        double total = numbers[0];
+
+        for(int i = 1; i < numbers.length; i++){
+            total -= numbers[i];
+        }
+
+        return (total);
     }
 
-    public static String doOperation(String option, double a, double b){
+    public static double mean(double[] numbers){
+        double total = 0;
+        for(int i = 0; i < numbers.length; i++){
+            total+=numbers[i];
+        }
+        double mean = total/numbers.length;
+
+        return(mean);
+    }
+
+    public static double median(double[] numbers){
+        Arrays.sort(numbers);
+        double arraySize = numbers.length;
+        if(arraySize%2 == 0){
+            int middle = numbers.length / 2;
+            double median = (numbers[(middle)] + numbers[(middle-1)])/2;
+            return (median);
+        }
+        else{
+            double middle = (numbers.length/2);
+            double middleCeil = Math.ceil(middle);
+            int median = (int) numbers[(int) middleCeil];
+            return (median);
+        }
+
+    }
+
+    public static double mode(double[] numbers){
+        double max = Arrays.stream(numbers).max().getAsDouble();
+
+        int t = (int) (max + 1);
+        double[] count = new double[t];
+        for (int i = 0; i < t; i++)
+        {
+            count[i] = 0;
+        }
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            count[(int) numbers[i]]++;
+        }
+
+        int mode = 0;
+        double k = count[0];
+        for (int i = 1; i < t; i++)
+        {
+            if (count[i] > k)
+            {
+                k = count[i];
+                mode = i;
+            }
+        }
+
+        return mode;
+    }
+
+    public static double range(double[] numbers){
+        Arrays.sort(numbers);
+        return (numbers[numbers.length-1] - numbers[0]);
+    }
+
+    public static double standardDeviation(double[] numbers){
+        double sum = 0.0, standardDeviation = 0.0;
+        int length = numbers.length;
+
+        for(double num : numbers) {
+            sum += num;
+        }
+
+        double mean = sum/length;
+
+        for(double num: numbers) {
+            standardDeviation += Math.pow(num - mean, 2);
+        }
+
+        return Math.sqrt(standardDeviation/length);
+    }
+
+    public static double percentage(double[] numbers){
+        double percentage = numbers[0] / 100;
+        double number = numbers[1];
+        return (percentage * number);
+    }
+
+    public static double exponent(double[] numbers){
+        double exponentValue;
+        exponentValue = Math.pow(numbers[0], numbers[1]);
+        return(exponentValue);
+    }
+
+    public static double pythagorean(double[] numbers){
+        double a = Math.pow(numbers[0], 2);
+        double b = Math.pow(numbers[1], 2);
+        double c = Math.sqrt(a+b);
+        return c;
+    }
+
+    public static double root(double[] numbers){
+        double root = Math.pow((numbers[1]), 1/numbers[0]);
+        return root;
+    }
+
+
+    public static String doOperation(String option, double[] numbers ){
         option = option.toLowerCase();
         double answer;
         switch (option){
             case("add"):
-                answer = add(a,b);
-                return(a + " + " + b + " = " + answer);
+                answer = add(numbers);
+                return("="+answer);
             case("multiply"):
-                answer = multiply(a,b);
-                return(a + " * " + b + " = " + answer);
+                answer = multiply(numbers);
+                return("="+answer);
             case("divide"):
-                answer = divide(a,b);
-                return(a + " / " + b + " = " + answer);
+                answer = divide(numbers);
+                return("="+answer);
             case("subtract"):
-                answer = subtract(a,b);
-                return(a + " - " + b + " = " + answer);
+                answer = subtract(numbers);
+                return("="+answer);
+            case("mean"):
+                answer = mean(numbers);
+                return("="+answer);
+            case("median"):
+                answer = median(numbers);
+                return("="+answer);
+            case("mode"):
+                answer = mode(numbers);
+                return("="+answer);
+            case("range"):
+                answer = range(numbers);
+                return("="+answer);
+            case("standard deviation"):
+                answer = standardDeviation(numbers);
+                return("="+answer);
+            case("percentage"):
+                answer = percentage(numbers);
+                return("="+answer);
+            case("exponent"):
+                answer = exponent(numbers);
+                return("="+answer);
+            case("pythagorean"):
+                answer = pythagorean(numbers);
+                return("="+answer);
+            case("root"):
+                answer = root(numbers);
+                return("="+answer);
             default:
-                return("INVALID OPERATION");
+                return ("error");
+
         }
     }
     public static void main(String args[]){
+        // initializing variables
+        int amountOfNumbers = 0;
+
         Scanner operation = new Scanner(System.in);
         System.out.println("PLEASE ENTER WHAT OPERATION YOU WOULD LIKE TO DO: ");
-        System.out.println("Options: ");
-        System.out.println("\tadd\n\tsubtract\n\tmultiply\n\tdivide ");
+        System.out.println("Type \"help\" to see options");
         String option = operation.next();
 
-        Scanner x = new Scanner(System.in);
-        System.out.println("Enter in a number: ");
-        double a = x.nextDouble();
+        option.toLowerCase();
+        if(option.equals("help")){
+            do{
+                System.out.println("PLEASE NOTE: INPUTS ARE UP TO 10 VALUES (for some cases)");
+                System.out.println("\n\tadd:\n\t\tThis will allow you to add two or more numbers");
+                System.out.println("\n\tsubtract:\n\t\tThis will allow you to subtract two or more numbers");
+                System.out.println("\n\tmultiply:\n\t\tThis will allow you to multiply two or more numbers");
+                System.out.println("\n\tdivide:\n\t\tThis will allow you to divide two or more numbers");
+                System.out.println("\n\tmean:\n\t\tThis will you to find the mean in a list of numbers");
+                System.out.println("\n\tmedian:\n\t\tThis will you to find the median in a list of numbers");
+                System.out.println("\n\tmode:\n\t\tThis will you to find the mode in a list of numbers");
+                System.out.println("\n\trange:\n\t\tThis will you to find the range in a list of numbers");
+                System.out.println("\n\tstandard deviation:\n\t\tThis will you to find the standard deviation in a list of numbers");
+                System.out.println("\n\tpercentage:\n\t\tThis will you to find the percent x of value y");
+                System.out.println("\n\texponent:\n\t\tThis will you to find x (first input) ^ y (second input)");
+                System.out.println("\n\tpythagorean:\n\t\tThis will you to find the pythagorean theorem using two numbers");
+                System.out.println("\n\troot:\n\t\tThis will you to nth-root using two numbers");
 
-        Scanner y = new Scanner(System.in);
-        System.out.println("Enter in another number: ");
-        double b = y.nextDouble();
+                System.out.println("PLEASE ENTER WHAT OPERATION YOU WOULD LIKE TO DO: ");
+                System.out.println("Type \"help\" to see options");
+                option = operation.next();
+            }while(option.equals("help"));
+        }
 
-        String answer = doOperation(option, a, b);
+        Scanner numberOfNumbers = new Scanner(System.in);
+        if(!(operation.equals("exponent") || operation.equals("pythagorean") || operation.equals("percentage") || operation.equals("root"))){
+            System.out.println("PLEASE ENTER HOW MANY NUMBERS YOU WOULD LIKE TO OPERATE ON (up to 10): ");
+        }
+        else{
+            System.out.println("PLEASE ENTER TWO NUMBERS ");
+        }
+        amountOfNumbers = numberOfNumbers.nextInt();
+
+        double[] numbers = new double[amountOfNumbers];
+
+        System.out.println("ENTER YOUR ARRAY OF NUMBERS: ");
+
+        Scanner list = new Scanner(System.in);
+        for(int i = 0; i < amountOfNumbers; i++){
+            numbers[i]=list.nextDouble();
+        }
+
+        String answer = doOperation(option, numbers);
 
         System.out.println(answer);
     }
---
+++ src/test/java/calculatortest.java
@@ -0,0 +1,118 @@
+import com.Calculator.calculator;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class calculatortest {
+    @Test
+    void testAdd(){
+        double[] arrayAdd = {1,2};
+        calculator c = new calculator();
+        assertEquals(3.0, c.add(arrayAdd));
+    }
+
+    @Test
+    void testMultiply(){
+        double[] arrayMultiply = {7,2,5,8};
+        calculator c = new calculator();
+        assertEquals(560.0, c.multiply(arrayMultiply));
+    }
+
+    @Test
+    void testSubtract(){
+        double[] arraySubtract = {1,2,9};
+        calculator c = new calculator();
+        assertEquals(-10.0, c.subtract(arraySubtract));
+    }
+
+    @Test
+    void testDivide(){
+        double[] arrayDivide = {20,2,4};
+        calculator c = new calculator();
+        assertEquals(2.5, c.divide(arrayDivide));
+    }
+
+    @Test
+    void testMedianOdd(){
+        double[] arrayMedian = {1, 2, 3, 4, 5};
+        calculator c = new calculator();
+        assertEquals(3, c.median(arrayMedian));
+    }
+
+    @Test
+    void testMedianEven(){
+        double[] arrayMedian = {1, 2, 3, 4, 5, 6};
+        calculator c = new calculator();
+        assertEquals(3.5, c.median(arrayMedian));
+    }
+
+    @Test
+    void testRange(){
+        double[] arrayRange = {8,9,12,90,2893,1};
+        calculator c = new calculator();
+        assertEquals(2892.0, c.range(arrayRange));
+    }
+
+    @Test
+    void testMode(){
+        double[] arrayMode = {1, 2, 3, 4, 2, 3, 2, 2, 2};
+        calculator c = new calculator();
+        assertEquals(2.0, c.mode(arrayMode));
+    }
+
+    @Test
+    void testStandardDeviation(){
+        double[] arrayRange = {8,9,12,90,2893,1};
+        calculator c = new calculator();
+        assertEquals(1069.642528552829, c.standardDeviation(arrayRange));
+    }
+
+    @Test
+    void testPythagorean(){
+        double[] arrayPythagorean = {6, 8};
+        calculator c = new calculator();
+        assertEquals(10.0, c.pythagorean(arrayPythagorean));
+    }
+
+    @Test
+    void testPercentage(){
+        double[] arrayPercentage = {6, 100};
+        calculator c = new calculator();
+        assertEquals(6.0, c.percentage(arrayPercentage));
+    }
+
+    @Test
+    void testRoot(){
+        double[] arrayRoot = {3,27};
+        calculator c = new calculator();
+        assertEquals(3.0, c.root(arrayRoot));
+    }
+
+    @Test
+    void testPythagoreanNegative(){
+        double[] arrayPythagorean = {6, -8};
+        calculator c = new calculator();
+        assertEquals(10.0, c.pythagorean(arrayPythagorean));
+    }
+
+    @Test
+    void testAddNull(){
+        double[] arrayAdd = {};
+        calculator c = new calculator();
+        assertEquals(0.0, c.add(arrayAdd));
+    }
+
+    @Test
+    void testAddNegativeNumbers(){
+        double[] arrayAdd = {-7,-2};
+        calculator c = new calculator();
+        assertEquals(-9.0, c.add(arrayAdd));
+    }
+
+    @Test
+    void testSubtractWithNegative(){
+        double[] arraySubtract = {1,-9};
+        calculator c = new calculator();
+        assertEquals(10.0, c.subtract(arraySubtract));
+    }
+
+}
